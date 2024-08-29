a2a07d6d660b0894c99ddc5e434c673b9f52bd7a
abc <soo990414@edu.dju.ac.kr>
2024-08-29 22:29:32 +0900

    로그인 ì성공O 로그인실패는 오류남

--- src/test/java/com/lss/board/BoardApplicationTests.java
+++ src/test/java/com/lss/board/BoardApplicationTests.java
@@ -1,13 +1,25 @@
 package com.lss.board;
 
+import com.lss.board.member.Member;
+import com.lss.board.member.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class BoardApplicationTests {
+	@Autowired
+	private MemberRepository memberRepository;
 
 	@Test
 	void contextLoads() {
+		Member member1 = new Member();
+		member1.setUserId("test");
+		member1.setPassword("test");
+		member1.setRegDate(LocalDateTime.now());
+		this.memberRepository.save(member1);
 	}
 
 }
